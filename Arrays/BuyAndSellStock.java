17f43fabc6ba9464b5840421fd04a298fd352197
Payal Sharma <payalbhimsharma03@gmail.com>
2026-06-30 19:27:23 +0530

    Best Time to Buy and Sell Stock

---
+++ Arrays/BuyAndSellStock.java
@@ -0,0 +1,17 @@
+class BuyAndSellStock{
+    public static void main(String args[]){
+        int[] prices = {7,1,5,3,6,4};
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 0 ; i < prices.length ; i++){
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            int profit = prices[i]-minPrice;
+            if(profit>maxProfit){
+                maxProfit = profit;
+            }
+    }
+    System.out.print("Maximum Pofit : " + maxProfit);
+}
+}
