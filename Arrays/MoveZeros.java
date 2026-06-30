2c84a223eced3c0d27ed308f176ae8c5d2abb727
Payal Sharma <payalbhimsharma03@gmail.com>
2026-06-30 09:30:08 +0530

    Move All Zeros to End

--- Arrays/MoveZeros.java
+++ Arrays/MoveZeros.java
@@ -1,16 +1,16 @@
 class MoveZeros{
     public static void main(String args[]){
         int[] arr = {0,1,0,3,12};
-        int index = 0;
+        int nonZeroindex = 0;
         for(int i = 0 ; i < arr.length ; i++){
             if(arr[i]!=0){
-                arr[index]=arr[i];
-                index++;
+                arr[nonZeroindex]=arr[i];
+                nonZeroindex++;
             }
         }
-        while(index < arr.length){
-               arr[index] = 0;
-               index++;
+        while(nonZeroindex < arr.length){
+               arr[nonZeroindex] = 0;
+               nonZeroindex++;
         }
         for(int i = 0 ; i < arr.length ; i++){
             System.out.print(arr[i]+" ");
