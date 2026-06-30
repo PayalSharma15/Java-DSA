6eafcaecafc6065bb8c001f6562a54ff9a533d15
Payal Sharma <payalbhimsharma03@gmail.com>
2026-06-30 08:17:16 +0530

    Largest Element in Array

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,12 +1,12 @@
 class LargestElement{
     public static void main(String args[]){
        int[] arr = {3, 7, 2, 9, 5};
-       int maxElement = arr[0];
+       int largestElement = arr[0];
        for(int i = 0 ; i < arr.length ; i++){
-          if(arr[i]>maxElement){
-            maxElement = arr[i];
+          if(arr[i]>largestElement){
+            largestElement = arr[i];
           }
        }
-       System.out.print(maxElement);
+       System.out.print(largestElement);
     }
 }
