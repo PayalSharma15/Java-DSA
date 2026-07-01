4b02835a0e2cd879f1f3e777b8e2bc46d0bd52a1
Payal Sharma <payalbhimsharma03@gmail.com>
2026-07-01 21:38:35 +0530

    Righ tTriangle Star
    Updated the formatting of the star output in RightTriangleStar.java.

--- Patterns/RightTriangleStar.java
+++ Patterns/RightTriangleStar.java
@@ -3,9 +3,17 @@ class RightTriangleStar{
         int n = 5;
         for(int i = 1 ; i <= n ; i++){
           for(int j = 1 ; j <= i ; j++){
-            System.out.print(" * ");
+            System.out.print("*");
           }
         System.out.println();
         }
     }
 }
+/*
+Output:
+*
+**
+***
+****
+*****
+    */
