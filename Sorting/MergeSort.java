e3ea714e95adc13448776d11e48ca21d0d4e9d21
Payal Sharma <payalbhimsharma03@gmail.com>
2026-07-08 21:10:09 +0530

    Merge Sort

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,41 @@
+class MergeTwoSortedArrays {
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        int[] result = new int[n1 + n2];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < n1 && j < n2) {
+
+            if (arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            } else {
+                result[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            result[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            result[k] = arr2[j];
+            j++;
+            k++;
+        }
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
